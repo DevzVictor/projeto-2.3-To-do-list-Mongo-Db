c7b69cc5687f1cf2ad2fb687e7366e5e7fbe3674
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-11-04 21:02:28 -0300

    feat: cors debug 2

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const connectToDatabase = require('./src/database/database');
 
 connectToDatabase();
 
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/tarefas', routes);
 app.use('/api-docs', swaggerRoutes);
